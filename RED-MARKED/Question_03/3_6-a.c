1ac33e5054248474efb5efedecdd3ada8b0c0d9e
saad <saad.csecu@gmail.com>
2024-01-03 16:35:38 +0600

    Modified

--- RED-MARKED/Question_03/3_6-a.c
+++ RED-MARKED/Question_03/3_6-a.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  int a=5,b=5;
+  int const a=5,b=6;
 
-  if(a=b)
+  //if(a=b)
+  if(a==b)
   {
 
     printf("The if statement is executed");
@@ -18,6 +19,10 @@ int main()
 
   return 0;
 }
-/* if any if statement is given like if(a=b),then it will execute when 
- * the value of both a and b would be assigned before this statement.
- * */
+/*The condition  will be exicuted when we put '==' instead of '='.
+This basically compares two value of 'a' and 'b'.If both values are equal,
+then the 'printf' statement executes.
+If we put a single equal sign(=),
+the statement checks if the value of 'b' can be or not store in 'a'.
+As both are same type variable,the if statement finds the condition true.
+So the printf function execute in this case also.*/
