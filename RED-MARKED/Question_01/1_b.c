d21dd87e4b0c7bb72288810155c7a647f5aa2a4c
EMRAN ALI <emran.ali.csecu@gmail.com>
2024-01-03 12:25:50 +0600

    Modified format spacifier

--- RED-MARKED/Question_01/1_b.c
+++ RED-MARKED/Question_01/1_b.c
@@ -2,11 +2,11 @@
 #include <stdbool.h>
 int main()
 {
-    printf("Size of Integer Data Type: %d Bytes\n", sizeof(int));
-    printf("Size of Float Data Type: %d Bytes\n", sizeof(float));
-    printf("Size of Double Data Type: %d Bytes\n", sizeof(double));
-    printf("Size of Long Data Type: %d Bytes\n", sizeof(long));
-    printf("Size of Character Data Type: %d Bytes\n", sizeof(char));
-    printf("Size of Integer Data Type: %d Bytes\n", sizeof(bool));
+    printf("Size of Integer Data Type: %zu Bytes\n", sizeof(int));
+    printf("Size of Float Data Type: %zu Bytes\n", sizeof(float));
+    printf("Size of Double Data Type: %zu Bytes\n", sizeof(double));
+    printf("Size of Long Data Type: %zu Bytes\n", sizeof(long));
+    printf("Size of Character Data Type: %zu Bytes\n", sizeof(char));
+    printf("Size of Boolean Data Type: %zu Bytes\n", sizeof(bool));
     return 0;
 }
